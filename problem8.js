8219eb5262126c3672f37680b34ee170bb334e09
yadeepak <deepaky19196@gmail.com>
2020-05-12 01:38:45 +0530

    flatland Space Station

---
+++ problem8.js
@@ -0,0 +1,14 @@
+// Complete the flatlandSpaceStations function below.
+function flatlandSpaceStations(n, m, c) {
+	const resultArray = [];
+	for (let i = 0; i < n; i++) {
+		const checkOverStations = [];
+		for (let j = 0; j < c.length; j++) {
+			checkOverStations.push(Math.abs(c[j] - i));
+		}
+		const minDistance = Math.min.apply(null, checkOverStations);
+		resultArray.push(Number(minDistance));
+	}
+	const maxDistance = Math.max.apply(null, resultArray);
+	return maxDistance;
+}
